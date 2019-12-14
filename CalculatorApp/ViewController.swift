153b4ffee4b11be61c9c3898c1c7a62b8e326ef6
Elena Kulakova <egeorgievska89@gmail.com>
2019-12-14 22:27:29 +0100

    Working in progress. Some functions implementing for calButton

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -13,13 +13,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayLabel: UILabel!
     var isFinishedTypeingNumber: Bool = true
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    private var displayValue: Double {
+        get {
+            guard let currentDisplayValue = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double")
+            }
+            return currentDisplayValue
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
     }
     
     @IBAction func calButtonPressed(_ sender: UIButton) {
-    
+        isFinishedTypeingNumber = true
+        
+       
+            if let calMethod = sender.currentTitle {
+            if calMethod == "+/-" {
+                displayLabel.text = String(displayValue * -1)
+            } else if calMethod == "AC" {
+                self.displayLabel.text = "0"
+            } else if calMethod == "%" {
+                displayLabel.text = String(displayValue/100)
+            }
+        }
     }
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
@@ -29,6 +47,16 @@ class ViewController: UIViewController {
                 displayLabel.text = numberValue
                 isFinishedTypeingNumber = false
             } else {
+                
+                if numberValue == "." {
+                   
+                   let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                
                 displayLabel.text = (displayLabel.text ?? "") + numberValue
             }
         }
