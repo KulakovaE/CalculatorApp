146bfaf37e2d1b2b730a7c40f7f0489fdb20f58e
Elena Kulakova <egeorgievska89@gmail.com>
2019-12-14 12:36:18 +0100

    adding functionalists to append the buttons in the display label

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    var isFinishedTypeingNumber: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func calButtonPressed(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        if let numberValue = sender.currentTitle {
+            if isFinishedTypeingNumber {
+                displayLabel.text = numberValue
+                isFinishedTypeingNumber = false
+            } else {
+                displayLabel.text = (displayLabel.text ?? "") + numberValue
+            }
+        }
+    }
 }
 
